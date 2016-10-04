9845673c6d7596e1e750d7cf9c2b1d3bc7dc18a8
sevedge <jjesc1031@gmail.com>
2016-10-04 12:40:53 -0700

    2Nic ARM

--- 2nic/configurenic.sh
+++ 2nic/configurenic.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 # Configure Additional Nic's
-echo "Starting NIC Configuration for 2 NIC's'" >> /var/log/onboardnic.log
+echo "Starting NIC Configuration for 2 NIC Deployment" >> /var/log/onboardnic.log
 
-##Code to Add 
+## Set DB Variables to allow for multi nic
+    tmsh modify sys db provision.1nic value forced_enable
+    tmsh modify sys db provision.1nicautoconfig value disable
+    bigstart restart
+## Check bigstart tmm status in a while loop here instaed of a static sleep, but will use this for now
+    sleep 180
 
+## Modify this to loop based on parameter above based on # of NIC's
+    tmsh create net vlan external interfaces add { 1.0 { untagged }}
+    tmsh create net self external_ip address 10.0.1.5/24 vlan external allow-service default
+    tmsh create net route default gw 10.0.1.1
+    tmsh save sys config
 
-## End Script
+#reboot
+echo "Ending NIC Configuration for 2 NIC Deployment" >> /var/log/onboardnic.log
